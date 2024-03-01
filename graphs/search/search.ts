500ddbda5cf7f68f06599568e7d7c0e46db47f2e
Carl Molina <carlandrew.molina@gmail.com>
2024-02-29 18:12:49 -0800

    updated and improved docstring

--- graphs/search/search.ts
+++ graphs/search/search.ts
@@ -51,7 +51,7 @@ function iDfs(start: GNodeStr): string[] {
   return out;
 }
 
-/** Return array of nodes, in BFS order (recursive version)  */
+/** Return array of nodes, in BFS order */
 
 function bfs(start: GNodeStr): string[] {
 
