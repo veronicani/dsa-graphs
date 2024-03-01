758fe9018f2918b0e5512ee72161e42947ddd98b
Carl Molina <carlandrew.molina@gmail.com>
2024-02-29 17:31:43 -0800

    added first draft rDfs w/ passing tests

--- graphs/search/search.ts
+++ graphs/search/search.ts
@@ -1,19 +1,52 @@
 import { Queue } from "../common/queue";
 import { GNodeStr } from "../graph/graph";
+import { Stack } from "../common/stack";
 
 /** Return array of nodes, in DFS order (recursive version)  */
 
 function rDfs(
-    start: GNodeStr,
-    result: string[] = [],
-    visited = new Set([start])): string[] {
-  return ["todo"];
+  start: GNodeStr,
+  result: string[] = [],
+  visited = new Set([start])): string[] {
+
+  // base case
+  if (result.length === visited.size) return result;
+
+  result.push(start.value);
+
+  for (const n of start.adjacent) {
+    if (!visited.has(n)) visited.add(n);
+    rDfs(n, result, visited);
+  }
+
+  return result;
+
 }
 
 /** Return array of nodes, in DFS order (iterative version)  */
 
 function iDfs(start: GNodeStr): string[] {
-  return ["todo"];
+
+  const toVisit = new Stack<GNodeStr>([start]);
+  const seen = new Set<GNodeStr>();
+  const out: string[] = [];
+
+  while (!toVisit.isEmpty()) {
+    const curr = toVisit.pop();
+
+    if (!seen.has(curr)) {
+      seen.add(curr);
+      out.push(curr.value);
+    }
+
+    for (const n of curr.adjacent) {
+      if (!seen.has(n)) {
+        toVisit.push(n);
+      }
+    }
+  }
+
+  return out;
 }
 
 /** Return array of nodes, in BFS order (recursive version)  */
@@ -32,9 +65,12 @@ function bfs(start: GNodeStr): string[] {
       out.push(curr.value);
     }
 
-
+    for (const n of curr.adjacent) {
+      if (!seen.has(n)) {
+        toVisit.enqueue(n);
+      }
+    }
   }
-
 
   return out;
 }
