38b51be86d9179b34855123d48a46bf027eb7505
Carl Molina <carlandrew.molina@gmail.com>
2024-02-29 18:12:39 -0800

    added comments and TODOs

--- graphs/graph/graph.ts
+++ graphs/graph/graph.ts
@@ -21,6 +21,7 @@ class GraphStr {
 
   /** Add node to graph. */
   addNode(node: GNodeStr): void {
+    // TODO: don't need the logic gate here
     if (!this.nodes.has(node)) this.nodes.add(node);
   }
 
@@ -33,6 +34,7 @@ class GraphStr {
 
   /** Add edge between v1 and v2. */
   addEdge(v1: GNodeStr, v2: GNodeStr): void {
+    // TODO: don't need guard here
     if (!v1.adjacent.has(v2)) {
       v1.adjacent.add(v2);
       v2.adjacent.add(v1);
@@ -40,7 +42,8 @@ class GraphStr {
   }
 
   /** Add edges between v1 and v2. */
-  addEdges(nodes: GNodeStr[]): void {
+  // TODO: this is how you type this correctly
+  addEdges(nodes: [GNodeStr, GNodeStr][]): void {
     for (const [v1, v2] of nodes) {
       this.addEdge(v1, v2);
     }
@@ -48,6 +51,7 @@ class GraphStr {
 
   /** Remove edge between v1 and v2. */
   removeEdge(v1: GNodeStr, v2: GNodeStr): void {
+    // TODO: you can call delete without having to check if it exists
     if (v1.adjacent.has(v2)) {
       v1.adjacent.delete(v2);
       v2.adjacent.delete(v1);
@@ -57,10 +61,12 @@ class GraphStr {
   /** Remove node from graph. */
   removeNode(node: GNodeStr): void {
     for (const n of this.nodes) {
+      // TODO: don't need this guard
       if (n.adjacent.has(node)) {
         n.adjacent.delete(node);
       }
     }
+    // You HAVE to do this to clear the deleted node's adjacents
     node.adjacent.clear();
     this.nodes.delete(node);
   }
